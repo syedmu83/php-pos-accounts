75524e7d8727d491c78e4e58383070eec8c40d75
Syed Muhammad Farooq Asghar <syedmu83@gmail.com>
2017-07-28 00:39:20 +0500

    PhP Open Source POS and Accounting System

---
+++ application/language/english/locations_lang.php
@@ -0,0 +1,62 @@
+<?php
+$lang['locations_location_id'] = 'Location ID';
+$lang['locations_name'] = 'Name';
+$lang['locations_address'] = 'Address';
+$lang['locations_phone'] = 'Phone';
+$lang['locations_fax'] = 'Fax';
+$lang['locations_email'] = 'Email';
+$lang['locations_new'] = 'New Location';
+$lang['locations_update'] = 'Update Location';
+$lang['locations_delete'] = 'Delete';
+$lang['locations_cleanup_old_locations'] = 'Clean up Old Locations';
+$lang['locations_basic_information'] = 'Location Information';
+$lang['locations_successful_adding'] = 'You have successfully added location';
+$lang['locations_successful_updating'] = 'You have successfully updated location';
+$lang['locations_successful_deleted'] = 'You have successfully deleted';
+$lang['locations_error_adding_updating'] = 'Error adding/updating location';
+$lang['locatoins_name_required'] = 'Location Name is required';
+$lang['locations_phone_required'] = 'Phone number is required';
+$lang['locations_phone_valid'] = 'Phone number should be valid';
+$lang['locations_confirm_delete'] = 'Are you sure you want to delete the selected locations?';
+$lang['locations_successful_deleted'] = 'You have successfully deleted';
+$lang['locations_one_or_multiple'] = 'location(s)';
+$lang['locations_cannot_be_deleted'] = 'Could not deleted selected locations';
+$lang['locations_adding_location_requires_addtional_license'] = 'Adding another location requires another license';
+$lang['locations_confirm_purchase'] = 'Have you purchased an additional license for this location?';
+$lang['locations_purchase_email'] = 'Purchase E-Mail';
+$lang['locations_invalid_email_or_dont_have_auth'] = 'You have entered an invalid email or do not have the required licenses to add this location. Please contact support@phpsoftwares.com if you believe this is an error.';
+$lang['locations_purchase_additional_licenses'] = 'Purchase Additional Licenses';
+//===============================================================================================================
+$lang['vehicle_id'] = 'Vehicle ID';
+$lang['manufacturer_id'] = 'Manufacturer ID';
+$lang['vehicle_number'] = 'Vehicle Number';
+$lang['vehicle_manufacturer'] = 'Manufacturer';
+$lang['vehicle_color'] = 'Color';
+$lang['vehicle_model'] = 'Model';
+$lang['vehicle_year'] = 'Year';
+$lang['vehicle_reg_no'] = 'Reg.No';
+$lang['vehicle_engine_no'] = 'Engine No';
+$lang['vehicles_chassis_no'] = 'Chassis No';
+$lang['vehicle_insurance_no'] = 'Insurance No';
+$lang['vehicles_reg_expiry_date'] = 'Reg Expiry Date';
+$lang['vehicle_insurance_expiry_date'] = 'Insurance Expiry Date';
+$lang['vehicle_insurance_company'] = 'Insurance Company';
+$lang['vehicle_description'] = 'Description';
+
+$lang['vehicles_new'] = 'New Vehicle';
+$lang['vehicles_update'] = 'Update Vehicle';
+$lang['vehicles_delete'] = 'Delete';
+//$lang['vehicles_name_required'] = 'Location Name is required';
+//$lang['vehicles_phone_required'] = 'Phone number is required';
+//$lang['vehicles_phone_valid'] = 'Phone number should be valid';
+//$lang['vehicles_confirm_delete'] = 'Are you sure you want to delete the selected vehicles?';
+//$lang['vehicles_successful_deleted'] = 'You have successfully deleted';
+//$lang['vehicles_one_or_multiple'] = 'location(s)';
+//$lang['vehicles_cannot_be_deleted'] = 'Could not deleted selected vehicles';
+//$lang['vehicles_adding_location_requires_addtional_license'] = 'Adding another location requires another license';
+//$lang['vehicles_confirm_purchase'] = 'Have you purchased an additional license for this location?';
+//$lang['vehicles_purchase_email'] = 'Purchase E-Mail';
+//$lang['vehicles_invalid_email_or_dont_have_auth'] = 'You have entered an invalid email or do not have the required licenses to add this location. Please contact support@phpsoftwares.com if you believe this is an error.';
+//$lang['vehicles_purchase_additional_licenses'] = 'Purchase Additional Licenses';
+
+?>
